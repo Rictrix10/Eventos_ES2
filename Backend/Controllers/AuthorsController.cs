883e12c459e4bf6c2c47df99d6423154fcb166e8
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-12 16:44:14 +0100

    Alguns ajustes

--- Backend/Controllers/AuthorsController.cs
+++ Backend/Controllers/AuthorsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BusinessLogic.Context;
 using BusinessLogic.Entities;
+using BusinessLogic.Models;
 
 namespace Backend.Controllers
 {

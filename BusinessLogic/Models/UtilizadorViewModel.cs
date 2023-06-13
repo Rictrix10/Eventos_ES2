f272d3e50d17928388eafc37e79fc822aab75da5
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-13 17:46:55 +0100

    Criação das views

---
+++ BusinessLogic/Models/UtilizadorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models;
+
+public class UtilizadorViewModel
+{
+    public UtilizadorViewModel()
+    {
+        
+    }
+    
+    public UtilizadorViewModel(Utilizador utilizador)
+    {
+        Username = utilizador.Username;
+        Password = utilizador.Password;
+        Nome = utilizador.Nome;
+        Email = utilizador.Email;
+        Telefone = utilizador.Telefone;
+    }
+
+    public string? Telefone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Username { get; set; }
+}
---
+++ Frontend/Pages/Utilizadores.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessLogic.Context;
+using BusinessLogic.Models;
+namespace Frontend.Pages
+{
+    public class UtilizadoresModel : PageModel
+    {
+        private readonly EventosDBContext  _context;
+        public UtilizadoresModel()
+        {
+            _context = new EventosDBContext();
+        }
+        public List<UtilizadorViewModel> Utilizador { get;set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Utilizadors != null)
+            {
+                Utilizador = await _context.Utilizadors
+                    .Include(m => m.IdUtilizador)
+                    .Select(m => new UtilizadorViewModel(m)).ToListAsync();
+            }
+        }
+    }
+
+
+}

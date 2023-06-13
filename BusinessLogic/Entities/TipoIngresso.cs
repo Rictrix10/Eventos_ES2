dbde3223e73fda3459ec733d90089719e14a4f33
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-13 12:00:43 +0100

    Entidades adicionadas

---
+++ BusinessLogic/Entities/TipoIngresso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Entities;
+
+public partial class TipoIngresso
+{
+    public int IdTipoIngresso { get; set; }
+
+    public string? Nome { get; set; }
+
+    public decimal? Preco { get; set; }
+}

9640ca7e167ebd1419cc00d6db0c4a258448d172
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-15 11:36:33 +0100

    Fetch Events

---
+++ Backend/Controllers/EventsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BusinessLogic.Context;
+using BusinessLogic.Entities;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class EventsController : ControllerBase
+    {
+        
+        private readonly  EventosDBContext _context;
+
+        public EventsController(EventosDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Authors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<dynamic>>> GetEventos()
+        {
+            if (_context.Eventos == null)
+            {
+                return NotFound();
+            }
+
+            return await _context
+                .Eventos.Select(a => new
+                {   
+                    a.IdEvento,
+                    a.Nome,
+                    a.Data,
+                    a.Hora,
+                    a.Local,
+                    a.Descricao,
+                    a.Capacidademax,
+                    a.Categoria,
+                    Organizador = new {
+                        Nome = a.IdOrganizadorNavigation!.Nome ?? "sem organizador",
+                        
+                        Email = a.IdOrganizadorNavigation!.Email ?? "sem organizador"
+
+                    }
+                }).ToListAsync();
+            
+        }
+
+        // GET: api/Authors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> GetAuthor(Guid id)
+        {
+            if (_context.Eventos == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos.FindAsync(id);
+
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return evento;
+        }
+
+        // PUT: api/Authors/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthor(int id, Evento evento)
+        {
+            if (id != evento.IdEvento)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(evento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Authors
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Evento>> PostAuthor(Evento evento)
+        {
+            if (_context.Eventos == null)
+            {
+                return Problem("Entity set 'ES2DbContext.Authors'  is null.");
+            }
+
+            _context.Eventos.Add(evento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEventos", new { id = evento.IdEvento }, evento);
+        }
+
+        // DELETE: api/Authors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(Guid id)
+        {
+            if (_context.Eventos == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Eventos.Remove(evento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EventoExists(int id)
+        {
+            return (_context.Eventos?.Any(e => e.IdEvento == id)).GetValueOrDefault();
+        }
+    }
+    
+}

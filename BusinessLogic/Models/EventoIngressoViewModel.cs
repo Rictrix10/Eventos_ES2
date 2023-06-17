410256515d55e9f3c5082dfaf0551cb1c7ecad01
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-17 17:53:49 +0100

    Pagina de registar ingressos

---
+++ BusinessLogic/Models/EventoIngressoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models;
+
+public class EventoIngressoViewModel
+{
+    public EventoIngressoViewModel()
+    {
+        
+    }
+    
+    public EventoIngressoViewModel(EventoIngresso eventoIngresso)
+    {   
+        IdIngresso = eventoIngresso.IdIngresso;
+        IdEvento = eventoIngresso.IdEvento;
+        Quantidade = eventoIngresso.Quantidade;
+        Preco = eventoIngresso.Preco;
+        Tipo_Ingresso = eventoIngresso.TipoIngresso;
+
+    }
+
+    public string? Tipo_Ingresso { get; set; }
+
+    public decimal? Preco { get; set; }
+
+    public int? Quantidade { get; set; }
+
+    public int IdEvento { get; set; }
+
+    public int IdIngresso { get; set; }
+}

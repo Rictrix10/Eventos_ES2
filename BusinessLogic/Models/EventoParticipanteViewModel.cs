86cd0c98fb373f8a937ff1ffcb6dd8ecc17c1b7d
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-17 12:57:25 +0100

    Filtrar os eventos a funcionar direito

---
+++ BusinessLogic/Models/EventoParticipanteViewModel.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLogic.Entities;
+using System;
+
+namespace BusinessLogic.Models
+{
+    public class EventoParticipanteViewModel
+    {
+        public EventoParticipanteViewModel()
+        {
+        }
+
+        public EventoParticipanteViewModel(Evento evento)
+        {
+            IdEvento = evento.IdEvento;
+            Nome = evento.Nome;
+            Data = evento.Data;
+            Hora = evento.Hora;
+            Local = evento.Local;
+            Descricao = evento.Descricao;
+            Capacidademax = evento.Capacidademax;
+            Categoria = evento.Categoria;
+        }
+
+        public int IdEvento { get; set; }
+        public int? Organizador { get; set; }
+        public string? Categoria { get; set; }
+        public int? Capacidademax { get; set; }
+        public string? Descricao { get; set; }
+        public string? Local { get; set; }
+        public TimeOnly? Hora { get; set; }
+        public DateOnly? Data { get; set; }
+        public string? Nome { get; set; }
+        public string? NomeOrganizador { get; set; }
+    }
+}

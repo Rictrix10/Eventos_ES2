24906ce173645794140fceea7bbae4e201383ef6
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-17 23:55:18 +0100

    Regista Atividades mas não tudo

---
+++ BusinessLogic/Models/AddAtividadeViewModel.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models;
+
+public class AddAtividadeViewModel
+{
+    public AddAtividadeViewModel()
+    {
+        
+    }
+    
+    public AddAtividadeViewModel(Atividade atividade)
+    {
+        Nome = atividade.Nome;
+        Data = atividade.Data.HasValue ? atividade.Data.Value : default;
+        HoraString = atividade.Hora.ToString();
+        Descricao = atividade.Descricao;
+        IdEvento = atividade.IdEvento;
+        /*
+        Organizador = evento.IdOrganizador;
+        NomeOrganizador = evento.IdOrganizadorNavigation?.Nome;
+        */
+
+    }
+
+    public int? IdEvento { get; set; }
+    
+    
+    public string HoraString { get; set; }
+    
+
+    public string? Descricao { get; set; }
+    
+
+    public TimeOnly Hora
+    {
+        get
+        {
+            return TimeOnly.Parse(HoraString);
+        }
+    }
+
+    public DateOnly? Data { get; set; }
+
+// Dentro do construtor
+    
+
+    public string? Nome { get; set; }
+    
+
+
+}

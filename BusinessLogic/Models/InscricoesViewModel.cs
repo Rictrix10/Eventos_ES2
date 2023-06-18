81f82b69092e7e1707872cd1f3cf32a462acfbe1
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-18 15:02:17 +0100

    Já inscreve e cancela um evento

---
+++ BusinessLogic/Models/InscricoesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models
+{
+    public class InscricoesViewModel
+    {
+        public InscricoesViewModel()
+        {
+
+        }
+
+        public InscricoesViewModel(InscricaoEvento inscricaoevento)
+        {   
+            IdInscricaoEvento = inscricaoevento.IdInscricaoEvento;
+            IdEventoNavigation = inscricaoevento.IdEventoNavigation?.Nome;
+            IdParticipanteNavigation = inscricaoevento.IdParticipanteNavigation?.Nome;
+            TipoIngresso = inscricaoevento.TipoIngresso;
+            IdParticipante = inscricaoevento.IdParticipante;
+            
+            
+        }
+
+        public int IdInscricaoEvento { get; set; }
+
+        public string? IdEventoNavigation { get; set; }
+        public string? IdParticipanteNavigation { get; set; }
+        
+        public string TipoIngresso { get; set; }
+        
+        
+        public int? IdParticipante { get; set; }
+        
+
+        
+        
+
+        
+        
+    }
+}

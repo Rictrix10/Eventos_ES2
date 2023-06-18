6ea2d622f35b3d4b4cb3c7b5576c0abf97a3ef99
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-18 22:00:01 +0100

    Envia mensagens

---
+++ BusinessLogic/Models/AddMensagemViewModel.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models
+{
+    public class AddMensagemViewModel
+    {
+        public AddMensagemViewModel()
+        {
+
+        }
+
+        public AddMensagemViewModel(Mensagem mensagem)
+        {   
+            IdMensagem = mensagem.IdMensagem;
+            Mensagem1 = mensagem.Mensagem1;
+            IdOrganizador = mensagem.IdOrganizador;
+            IdParticipante = mensagem.IdParticipante;
+            IdEvento = mensagem.IdEvento;
+            
+        }
+        
+        public int IdMensagem { get; set; }
+
+        public string? Mensagem1 { get; set; }
+        
+        public int? IdOrganizador { get; set; }
+        
+        public int? IdParticipante { get; set; }
+        
+        public int? IdEvento { get; set; }
+
+
+    }
+}

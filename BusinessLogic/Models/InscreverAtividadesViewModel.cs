acc1de9e835131f8bf6be7510b1b5442065f8f2f
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-06-21 15:13:32 +0100

    Já é possível inscrever em atividades

---
+++ BusinessLogic/Models/InscreverAtividadesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models
+{
+    public class InscreverAtividadesViewModel
+    {
+        public InscreverAtividadesViewModel()
+        {
+
+        }
+
+        public InscreverAtividadesViewModel(InscricaoAtividade inscricaoatividade)
+        {   
+            IdInscricaoAtividade = inscricaoatividade.IdInscricaoAtividade;
+            IdParticipante = inscricaoatividade.IdParticipante;
+            IdAtividade = inscricaoatividade.IdAtividade;
+        }
+
+        public int IdInscricaoAtividade { get; set; }
+
+        public int? IdParticipante { get; set; }
+
+        public int? IdAtividade { get; set; }
+    }
+}
